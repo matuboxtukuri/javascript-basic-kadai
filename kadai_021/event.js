2c86a61ad67f4f1ab89cbc889576710e655614a0
matuboxtukuri <161719469+matuboxtukuri@users.noreply.github.com>
2024-07-05 21:51:09 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,13 @@
+const text =document.getElementById('text');
+
+const btn =document.getElementById('btn');
+
+btn.addEventListener('click',() =>{
+
+  setTimeout(() =>{
+
+   text.textContent='ボタンをクリックしました';
+
+  }, 2000);
+  
+});
